0f913f91629c7ba03c9b310bd6b5bc470b69a34b
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-05-09 18:29:07 +0300

    feat: add basic endpoints

---
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -0,0 +1,7 @@
+namespace GameStore.Api.Dtos;
+public record CreateGameDto(
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,72 @@
+using GameStore.Api.Dtos;
+
+const string GetNameEndpointName = "GetGame";
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/games", () => "Hello world!");
+List<GameDto> games = [
+    new (1, "Street Fighter 2", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new (2, "Mortal Kombat 10", "Fighting", 29.99M, new DateOnly(2015, 4, 14)),
+    new (3, "Grand Thet Auto 5", "Action-adventure", 19.99M, new DateOnly(2013, 9, 17))
+];
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/1
+app.MapGet("/games/{id}", (int id) =>
+{
+    var game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+   .WithName(GetNameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetNameEndpointName, new { id = game.Id }, game);
+});
+
+// PUT /games/1
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("/games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
 
 app.Run();
